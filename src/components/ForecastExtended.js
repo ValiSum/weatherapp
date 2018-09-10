1af6c915234197f7c7c552e3be14efc36d5a9fdf
Vali Sumurduc <vali_sum@outlook.com>
2018-09-10 21:06:03 +0200

    ForecastExtended 1

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './ForecastItem';
+import './styles.css';
 
 class ForecastExtended extends Component{
 
   render() {
     const { city } = this.props;
-    return (<div>Pronostico extendido de {city}</div>);
+
+    return (
+      <div>
+        <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
+        <ForecastItem weekDay={'Lunes'}/>
+      </div>
+    );
   }
 }
 
