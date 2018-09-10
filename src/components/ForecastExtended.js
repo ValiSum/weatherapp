cbdff73f33f0b09fc708c13f734d2c65bac6e706
Vali Sumurduc <vali_sum@outlook.com>
2018-09-10 23:17:36 +0200

    Forecast Extended 2

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -2,16 +2,62 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css';
+import transformWeather from "../services/transformWeather";
 
-class ForecastExtended extends Component{
+/*
+const days = [
+  'Lunes',
+  'Martes',
+  'Miercoles',
+  'Jueves',
+  'Viernes'
+];
+
+const data = {
+  temperature: 10,
+  humidity: 10,
+  weatherState: 'normal',
+  wind: 'normal'
+};
+*/
+
+const api_key = '10b496ad45e010a92b2dc139a87c9f74';
+const url = 'https://api.openweathermap.org/data/2.5/forecast';
+
+class ForecastExtended extends Component {
+
+  constructor() {
+    super();
+    this.state = { forecastData: null}
+  }
+
+  componentDidMount() {
+    const api_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+    fetch(api_forecast).then( data_api => {
+      return data_api.json();
+    }).then( weather_data => {
+      console.log(weather_data);
+    });
+  }
+
+  renderForecastItemDays() {
+    return "Render Items";
+    //return days.map( day => <ForecastItem key={day} weekDay={day} hour={10} data={data}/> )
+  }
+
+  renderProgress = () => {
+    return <h3>Cargando Pronóstico extendido...</h3>;
+  };
 
   render() {
     const { city } = this.props;
-
+    const { forecastData } = this.state;
     return (
       <div>
         <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
-        <ForecastItem weekDay={'Lunes'}/>
+        {forecastData ?
+          this.renderForecastItemDays() : this.renderProgress()
+        }
       </div>
     );
   }
